7a71934a25281f647523ab5d96e05be280fce96c
Niha Ahmad <nihaahmad26@gmail.com>
2021-12-07 15:09:14 -0600

    commit change

--- book_project/settings.py
+++ book_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*44)ga9@o(p3230b+bplu9fg151wh#j6a8fu&u^6tpg4y(yy6f'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
